54e86b719ccc1a1bbdc3d420c990a010e7a40a97
SagWel <nicolasbensault@free.fr>
2026-03-03 11:45:57 +0100

    Switch button implemanted

--- client/src/Pages/PageConnexions.jsx
+++ client/src/Pages/PageConnexions.jsx
@@ -1,17 +1,30 @@
 import '../styles.css'
 import FormulaireInscription from '../Components/FormulaireInscription'
+import { useState } from 'react'
 // import FormulaireConnexion from '../Components/FormulaireConnexion'
 
 const PageConnexions = () => {
+    const [loginRegister, setloginRegister] = useState("login")
+
+    const handleOnclick = () => {
+        if (loginRegister === "login") {
+            setloginRegister("register")
+        } else {
+            setloginRegister("login")
+        }
+    }
     return (
         <div className="flex items-center justify-center flex-col 
         size-full text-center mt-12 gap-30">
-        <h1 className='text-gray-50 text-4xl'>Sticky Pocket</h1>
-            <div className='flex flex-column justify-around gap-30'>
-                {/* <FormulaireConnexion /> */}
-                <FormulaireInscription />
+            <h1 className='text-gray-50 text-4xl'>Sticky Pocket</h1>
+            <div>
+                {loginRegister === "login" ? <div></div> : <FormulaireInscription />}
             </div>
-    </div>
+            <button type='button'className="w-full max-w-[512px] mt-4 bg-blue-600 text-white py-8 rounded-full hover:bg-blue-700 transition"
+            onClick={handleOnclick}>
+                <span className='text-2xl'>{loginRegister === "login" ? "Registering" : "Login"}</span>
+            </button>
+         </div>
 )
 }
 
