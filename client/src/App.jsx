4731b6f74662ff99fc33f183de843a69c1131206
Evangéline <herbrik.evangeline1997@gmail.com>
2026-03-03 09:35:55 +0100

    Maj from Main

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,10 @@
 import { Routes, Route } from "react-router-dom"
 import PageConnexions from "./Pages/PageConnexions"
-import PageLogin from "./Pages/PageLogin"
-import PageRegister from "./Pages/PageRegister"
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<PageConnexions />} />
-      <Route path="/login" element={<PageLogin />} />
-      <Route path="/register" element={<PageRegister />} />
     </Routes>
   )
 }
