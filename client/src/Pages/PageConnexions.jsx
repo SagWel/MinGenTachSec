407beb761dffde45ee49378070a13a841e1194ac
SagWel <nicolasbensault@free.fr>
2026-03-03 10:15:00 +0100

    Add font

--- client/src/Pages/PageConnexions.jsx
+++ client/src/Pages/PageConnexions.jsx
@@ -1,14 +1,18 @@
 import '../styles.css'
+import FormulaireInscription from '../Components/FormulaireInscription'
+// import FormulaireConnexion from '../Components/FormulaireConnexion'
 
 const PageConnexions = () => {
     return (
-    <div className="flex items-center justify-center flex-column 
-    size-full text-center">
-        <p className=''>Sticky Pocket</p>
-        <div className='flex flex-column justify-around gap-3'>
-        <h1>Sticky Pocket</h1>
-        </div>
+        <div className="flex items-center justify-center flex-col 
+        size-full text-center mt-12 gap-30">
+        <h1 className='text-gray-50 text-4xl'>Sticky Pocket</h1>
+            <div className='flex flex-column justify-around gap-30'>
+                {/* <FormulaireConnexion /> */}
+                <FormulaireInscription />
+            </div>
     </div>
 )
 }
+
 export default PageConnexions
