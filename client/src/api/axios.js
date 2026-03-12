821dd3a2079b16afb2787c46a46b68cbff2f44fd
DorineCo <dorinecoquette@hotmail.com>
2026-03-12 14:22:04 +0100

    axios credentials changed to true

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
   // withCredentials: true, //envoie automatiquement les cookies d'auth au serveur
-  withCredentials: 'include',
+  withCredentials: true,
   headers: {
     'Content-Type': 'application/json'
   }
